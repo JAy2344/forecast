de26e08e3460fb92bac3985cd7034040d4b9c910
Ayanda Dlamini <116947552+JAy2344@users.noreply.github.com>
2025-08-21 11:25:40 +0200

    Add API call for city weather search
    Introduced searchCity and updateWeather functions to fetch and log weather data from the SheCodes API when a city is searched. The handleSearch function now triggers the API call with the entered city name.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function updateWeather(response) {
+console.log(response.data);
+}
+
+
+
+function searchCity(city) {
+      //call API
+    let apiKey = "ad28f3a0557d8t5f574o89b184356e5a"
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input');
     let cityElement = document.querySelector('.weather-app-city');
     cityElement.innerHTML=searchInput.value;
-  //call API
+    searchCity(searchInput.value);
+
   //Search For city
 }
 
